da9418c08a0f0c29f9be96c3760852a10f4fc768
AswAstropyYt <117359303+AswAstropyYt@users.noreply.github.com>
2022-11-03 14:51:11 +0300

    Add files via upload

---
+++ additionfunctions.py
@@ -0,0 +1,50 @@
+
+import random
+import time
+import tkinter
+from asyncore import  loop
+class functions:
+    def mix(var1, var2):
+
+        return var1 + var2 + var.rl
+    def combine(strvl1, strvl2):
+
+        return str(strvl1+strvl2)
+    def wait(sec):
+        time.sleep(sec)
+        print(f'{sec} seconds Python waited')
+    def divide_the_text(text):
+
+        return print(f'{text}'.split())
+    def enter_to_winpage(enter, wintk):
+        enter = input('Hey we heard you wanna enter the winPage,\n now you need write what you want:\n')
+        win = wintk
+
+
+        if enter == True or 'True':
+            win.mainloop()
+
+
+
+
+
+
+
+
+class var:
+    rl = random.randint(1, 6)
+    a = 12
+    b = 7
+    n = 'Never gone give you Up! \n'
+    l = 'Never gone let you Up!'
+class subjects:
+    print(f"mixed numbers result is: \n{functions.mix(var1=var.a,var2=var.b)} , random mixer number is {var.rl}")
+    print(f"combined text result is: \n{functions.combine(strvl1=var.n,strvl2=var.l)}")
+    functions.divide_the_text(text=f'Bro Guy lol')
+    wintkv = tkinter.Tk()
+    functions.enter_to_winpage(enter=None, wintk=wintkv)
+
+class Useingfunctions:
+    functions.wait(sec=2)
+
+
